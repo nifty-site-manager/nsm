2caa297f361073bbf503415ccab79f52d3d04ad0
Mamadou Babaei <info@babaei.net>
2021-04-11 17:45:11 +0200

    fall back to /dev/urandom on freebsd 11.x, or else the build fails on 11.x

--- LuaJIT/src/lj_prng.c
+++ LuaJIT/src/lj_prng.c
@@ -109,6 +109,7 @@ static PRGR libfunc_rgr;
 #include <sys/syscall.h>
 #else
 
+#ifndef LJ_TARGET_HAS_GETENTROPY
 #if LJ_TARGET_OSX && !LJ_TARGET_IOS
 /*
 ** In their infinite wisdom Apple decided to disallow getentropy() in the
@@ -123,6 +124,7 @@ static PRGR libfunc_rgr;
 #endif
 #elif (LJ_TARGET_BSD && !defined(__NetBSD__)) || LJ_TARGET_SOLARIS || LJ_TARGET_CYGWIN
 #define LJ_TARGET_HAS_GETENTROPY	1
+#endif
 #endif
 
 #if LJ_TARGET_HAS_GETENTROPY
